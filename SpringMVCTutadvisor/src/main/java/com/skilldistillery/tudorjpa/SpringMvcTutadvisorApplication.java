2d9ed0246fda35a77fbdfb91d031ead99885f289
Jesse <Jesse.mena4@gmail.com>
2019-03-07 13:58:32 -0700

    Update: gradle update

--- SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/SpringMvcTutadvisorApplication.java
+++ SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/SpringMvcTutadvisorApplication.java
@@ -2,6 +2,7 @@ package com.skilldistillery.tudorjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.tudorjpa")
