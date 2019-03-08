3e7c8f1ae0d31d7c6101844cd868466e456419cb
Jesse <Jesse.mena4@gmail.com>
2019-03-08 11:08:25 -0700

    Update: updated a generic TutadvisorController layout

--- SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/controllers/TutadvisorController.java
+++ SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/controllers/TutadvisorController.java
@@ -2,6 +2,9 @@ package com.skilldistillery.tudorjpa.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.tudorjpa.data.TutDAO;
 
@@ -10,4 +13,14 @@ public class TutadvisorController {
 
 	@Autowired
 	private TutDAO tutDAO;
+ 
+//	*************PLACE HOLDER FOR ACTUAL CONTROLLER**************
+	@RequestMapping(path = { "/", "home.do" }, method = RequestMethod.GET)
+	public String index(Model model) {
+
+		model.addAttribute("addTudors", tutDAO.findALL());
+
+		return "WEB-INF/index.jsp";
+	}
+
 }
