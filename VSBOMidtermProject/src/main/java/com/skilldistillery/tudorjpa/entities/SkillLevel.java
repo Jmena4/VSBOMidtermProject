043975a7d8cc119eb44207bdef5a82dda460edcd
Jesse <Jesse.mena4@gmail.com>
2019-03-09 12:30:23 -0700

    Feat: created new SkillLevel.java file

---
+++ VSBOMidtermProject/src/main/java/com/skilldistillery/tudorjpa/entities/SkillLevel.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.tudorjpa.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SkillLevel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "skill_level_id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+//this is only a test
