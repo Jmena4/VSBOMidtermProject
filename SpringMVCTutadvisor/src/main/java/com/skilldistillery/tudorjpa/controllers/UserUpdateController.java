824c0627e28b06b8f76423ff4ca5191cfd2ce60d
markmahowald <markmahowald0328@gmail.com>
2019-03-12 11:45:57 -0600

    Update: added form action to profile.jsp

---
+++ SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/controllers/UserUpdateController.java
@@ -0,0 +1,29 @@
+package com.skilldistillery.tudorjpa.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.tudorjpa.data.TutAdvisorClient;
+import com.skilldistillery.tudorjpa.data.TutDAOUser;
+import com.skilldistillery.tudorjpa.entities.Proposal;
+import com.skilldistillery.tudorjpa.entities.User;
+
+@Controller
+public class UserUpdateController {
+
+	@Autowired
+	private TutDAOUser userDAO;
+
+//	@RequestMapping(path = "studentSkill.do", method = RequestMethod.GET)
+	
+
+	
+}
