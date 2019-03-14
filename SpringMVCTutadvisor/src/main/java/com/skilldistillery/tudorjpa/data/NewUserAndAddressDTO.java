f7f149a88f46fa4f059d26cf93e55c234ef2097b
markmahowald <markmahowald0328@gmail.com>
2019-03-13 18:48:45 -0600

    Feat: new user regestration now works.

---
+++ SpringMVCTutadvisor/src/main/java/com/skilldistillery/tudorjpa/data/NewUserAndAddressDTO.java
@@ -0,0 +1,140 @@
+package com.skilldistillery.tudorjpa.data;
+
+
+public class NewUserAndAddressDTO {
+	
+//	user fields
+
+	private String firstname;
+
+	private String lastname;
+
+	private String email;
+
+	private String phone;
+
+	private String username;
+
+	private String password;
+
+	private String url;
+	
+//	address fields
+
+
+	private String street;
+
+	private String street2;
+
+	private String city;
+
+	private String state;
+
+	private String zip;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getStreet2() {
+		return street2;
+	}
+
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+
+
+
+
+//	getters & setters
+	
+	
+}
